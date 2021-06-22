42fde4141c73be95deab32294d04b6aa7c460833
JD Infinity Apps <69279437+AdminAntidoto@users.noreply.github.com>
2021-06-22 14:15:39 -0400

    delete shopify

--- common_connector_library/models/stock_quant_package.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-
-class StockQuantPackage(models.Model):
-    _inherit = 'stock.quant.package'
-
-    tracking_no = fields.Char("Additional Reference",
-                              help="This Field Is Used For The Store Tracking No")
