103da1cbed74fe2ad222d1ba025834eec819ad69
JD Infinity Apps <soporte@antidoto.co>
2021-06-23 12:48:53 -0400

    deleted

--- shopify_ept/models/stock_inventory.py
+++
@@ -1,6 +0,0 @@
-from odoo import models,fields,api,_
-
-class StockInventory(models.Model):
-    _inherit = "stock.inventory"
-
-    is_shopify_product_adjustment = fields.Boolean("Shopify Product Adjustment?")
