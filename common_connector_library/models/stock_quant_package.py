e54c988ce6f9c9a600b1fcfa8d8dba8a9476e8d7
JD Infinity Apps <soporte@antidoto.co>
2021-06-26 23:18:06 -0400

    delete

--- common_connector_library/models/stock_quant_package.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# See LICENSE file for full copyright and licensing details.
-from odoo import models, fields
-
-class StockQuantPackage(models.Model):
-    _inherit = 'stock.quant.package'
-
-    tracking_no = fields.Char("Additional Reference", help="This field is used for storing the tracking number.")
